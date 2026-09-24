63e10122bee81a1dd42a885d78ec30e668effa82
Shopnojo Sanyal <shopnojosanyal07@gmail.com>
2026-09-24 19:20:58 +0530

    Fix ScrittoDock Icon import

--- app/src/main/java/com/internship/scritto/components/ScrittoDock.kt
+++ app/src/main/java/com/internship/scritto/components/ScrittoDock.kt
@@ -14,7 +14,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.CheckCircle
